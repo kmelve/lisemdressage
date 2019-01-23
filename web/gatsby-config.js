fdd461b45a92b406a4162604f558860305f08010
Knut Melvær <knut.melvaer@gmail.com>
2019-01-23 22:35:59 +0100

    place in header

--- web/gatsby-config.js
+++ web/gatsby-config.js
@@ -15,7 +15,7 @@ module.exports = {
       options: {
         trackingId: 'UA-133124493-1',
         // Puts tracking script in the head instead of the body
-        head: false,
+        head: true,
         // Setting this parameter is optional
         anonymize: true,
         // Setting this parameter is also optional
